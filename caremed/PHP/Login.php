cf7752222f9c8e612ec3fe0953d8723aed693753
Fatbardh Kadriu <fatbardh.kadriu@gmail.com>
2019-04-09 13:24:35 +0200

    Update Login.php

--- caremed/PHP/Login.php
+++ caremed/PHP/Login.php
@@ -99,28 +99,6 @@ fieldset {
   resize: none;
 }
 
-#contact button[type="submit"] {
-  cursor: pointer;
-  width: 100%;
-  border: none;
-  background: #283290;
-  color: #FFF;
-  margin: 0 0 5px;
-  padding: 10px;
-  font-size: 15px;
-}
-
-#contact button[type="submit"]:hover {
-  color :#20d34a	;
-  -webkit-transition: background 0.3s ease-in-out;
-  -moz-transition: background 0.3s ease-in-out;
-  transition: background-color 0.3s ease-in-out;
-}
-
-#contact button[type="submit"]:active {
-  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.5);
-}
-
 #contact input:focus,
 #contact textarea:focus {
   outline: 0;
@@ -174,9 +152,27 @@ a:hover
 {
   color: #20d34a;
 }
+input[type="submit"]{
+  cursor: pointer;
+  width: 100%;
+  border: none;
+  background: #283290;
+  color: #FFF;
+  margin: 0 0 5px;
+  padding: 10px;
+  font-size: 15px;}
+input[type="submit"]:hover{
+    color:#20d34a;
+    transition: background-color 0.3s ease-in-out;
+}
+input[type="submit"]:active{
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.5);
+
+}
 button a{
   color: white;
 }
+
     </style>
 </head>
 <body>
@@ -193,7 +189,7 @@ button a{
     </fieldset>
    
     <fieldset style="margin-top : 15px">
-      <button name="Request Appointment" type="submit" id="contact-submit" data-submit="...Sending">Login</button>
+    <input type="submit" name="submit" value="LOGIN"/>
     </fieldset>
     <div style="text-align:right">
     <p>Don't have an account?</p>
