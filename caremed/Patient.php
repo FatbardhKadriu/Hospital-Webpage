ef3a6f71641c1918a110d4771ca19bb63c71a808
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-04-23 17:08:37 +0200

    Riperdorimi i kodit permes include

--- caremed/Patient.php
+++ caremed/Patient.php
@@ -24,18 +24,18 @@
 
 	<div class="super_container">
 
-		<!-- Header -->
+	<!-- Header -->
 
-		<?php
-			include('header.php');
-		?>
+	<?php
+		include('header.php');
+	?>
 	
-		<!-- Home -->
+	<!-- Home -->
 
 		<div class="home">
 			<div class="home_slider_container">
 				<!-- Slider Progress -->
-				<div class="home_slider_progress">sdfdsfsdfs</div>
+				<div class="home_slider_progress"></div>
 			</div>
 			
 
@@ -273,9 +273,121 @@
 
 		<!-- Footer -->
 
-		<?php
-include('footer.php')
-?>
+
+		<footer class="footer">
+			<div class="footer_container">
+				<div class="container">
+					<div class="row">
+
+						<!-- Footer - About -->
+						<div class="col-lg-4 footer_col">
+							<div class="footer_about">
+								<div class="footer_logo_container">
+									<a href="#" class="d-flex flex-column align-items-center justify-content-center">
+										<div class="logo_content">
+											<div class="logo d-flex flex-row align-items-center justify-content-center">
+												<div class="logo_text">Care<span>Med</span></div>
+												<div class="logo_box">+</div>
+											</div>
+											<div class="logo_sub">Health Care Center</div>
+										</div>
+									</a>
+								</div>
+								<div class="footer_about_text">
+									<p>Hospitals are reservoirs of critical resources and knowledge. They can be
+										classified according
+										to the interventions they provide.</p>
+								</div>
+								<ul class="footer_about_list">
+									<li>
+										<div class="footer_about_icon"><img src="images/phone-call.svg" alt=""></div>
+										<span>+45 677 8993000 223</span>
+									</li>
+									<li>
+										<div class="footer_about_icon"><img src="images/envelope.svg" alt=""></div>
+										<span>caremed@gmail.com</span>
+									</li>
+									<li>
+										<div class="footer_about_icon"><img src="images/placeholder.svg" alt=""></div>
+										<span>Main Str. no 45-46, b3, 56832, Los Angeles, CA</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<!-- Footer - Links -->
+						<div class="col-lg-4 footer_col">
+							<div class="footer_links footer_column">
+								<div class="footer_title">Useful Links</div>
+								<ul>
+									<li><a href="#">Apply for a Job</a></li>
+									<li><a href="#">Terms & Conditions</a></li>
+									<li><a href="Patient.html">Patients</a></li>
+									<li><a href="#">Doctors</a></li>
+									<li><a href="#">News</a></li>
+									<li><a href="#">Contact</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<!-- Footer - News -->
+						<div class="col-lg-4 footer_col">
+							<div class="footer_news footer_column">
+								<div class="footer_title">Pharmacies</div>
+								<ul>
+									<li>
+										<div class="footer_news_title"><a href="https://farmaline.be">Farmaline
+												Pharmacy</a></div>
+
+									</li>
+									<li>
+										<div class="footer_news_title"><a href="https://drugs.com">Drugs Pharmacy</a>
+										</div>
+
+									</li>
+									<li>
+										<div class="footer_news_title"><a href="https://doz.pl">Doz Pharmacy</a></div>
+
+									</li>
+								</ul>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+			</div>
+			<div class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div
+								class="copyright_content d-flex flex-lg-row flex-column align-items-lg-center justify-content-start">
+								<div class="cr">
+									<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+									Copyright &copy;
+									<script>document.write(new Date().getFullYear());</script> All rights reserved |
+									Group 4 <i class="fa fa-heart-o" aria-hidden="true"></i>
+									<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								</div>
+								<div class="footer_social ml-lg-auto">
+									<ul>
+										<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
 
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="styles/bootstrap4/popper.js"></script>
--- caremed/header.php
+++ caremed/header.php
@@ -16,101 +16,108 @@
 </head>
 <body>
 
+<div class="super_container">
+
 <!-- Header -->
-<header class="header trans_200">
-		
-		<!-- Top Bar -->
-		<div class="top_bar">
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<div class="top_bar_content d-flex flex-row align-items-center justify-content-start">
-							
-							<div class="top_bar_item"><a href="PHP/login.php">Request an Appointment</a></div>
-							<div class="emergencies  d-flex flex-row align-items-center justify-content-start ml-auto">For Emergencies: +563 47558 623</div>
-						</div>
 
-					</div>
-				</div>
-			</div>
-		</div>
+	<!-- Header -->
 
-		<!-- Header Content -->
-		<div class="header_container">
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<div class="header_content d-flex flex-row align-items-center justify-content-start">
-							<nav class="main_nav ml-auto">
-								<ul>
-									<li><a href="index.php">Home</a></li>
-									<li><a href="about.php">About us</a></li>
-									<li><a href="doctors.php">Doctors</a></li>
-									<li><a href="services.php">Services</a></li>
-									<li><a href="Patient.php">Patient</a></li>
-									<li><a href="contact.php">Contact</a></li>
-								</ul>
-							</nav>
-							<div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+    <header class="header trans_200">
 
-		<!-- Logo -->
-		<div class="logo_container_outer">
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<div class="logo_container">
-							<a href="#">
-								<div class="logo_content d-flex flex-column align-items-start justify-content-center">
-									<div class="logo_line"></div>
-									<div class="logo d-flex flex-row align-items-center justify-content-center">
-										<div class="logo_text">Care<span>Med</span></div>
-										<div class="logo_box">+</div>
-									</div>
-									<div class="logo_sub">Health Care Center</div>
-								</div>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>	
-		</div>
+<!-- Top Bar -->
+<div class="top_bar">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="top_bar_content d-flex flex-row align-items-center justify-content-start">
 
-	</header>
+                    <div class="top_bar_item"><a href="PHP\login.php">Request an Appointment</a></div>
+                    <div
+                        class="emergencies  d-flex flex-row align-items-center justify-content-start ml-auto">
+                        For Emergencies: +563 47558 623</div>
+                </div>
 
-	<!-- Menu -->
+            </div>
+        </div>
+    </div>
+</div>
 
-	<div class="menu_container menu_mm">
+<!-- Header Content -->
+<div class="header_container">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="header_content d-flex flex-row align-items-center justify-content-start">
+                    <nav class="main_nav ml-auto">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About us</a></li>
+                            <li><a href="doctors.php">Doctors</a></li>
+                            <li><a href="services.php">Services</a></li>
+                            <li><a href="Patient.php">Patient</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-		<!-- Menu Close Button -->
-		<div class="menu_close_container">
-			<div class="menu_close"></div>
-		</div>
+<!-- Logo -->
+<div class="logo_container_outer">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="logo_container">
+                    <a href="#">
+                        <div
+                            class="logo_content d-flex flex-column align-items-start justify-content-center">
+                            <div class="logo_line"></div>
+                            <div class="logo d-flex flex-row align-items-center justify-content-center">
+                                <div class="logo_text">Care<span>Med</span></div>
+                                <div class="logo_box">+</div>
+                            </div>
+                            <div class="logo_sub">Health Care Center</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-		<!-- Menu Items -->
-		<div class="menu_inner menu_mm">
-			<div class="menu menu_mm">
-				<ul class="menu_list menu_mm">
-					<li class="menu_item menu_mm"><a href="index.php">Home</a></li>
-					<li class="menu_item menu_mm"><a href="about.php">About us</a></li>
-					<li class="menu_item menu_mm"><a href="services.php">Services</a></li>
-					<li class="menu_item menu_mm"><a href="news.php">News</a></li>
-					<li class="menu_item menu_mm"><a href="contact.php">Contact</a></li>
-				</ul>
-			</div>
-			<div class="menu_extra">
-				<div class="menu_appointment"><a href="#PHP/login.php">Request an Appointment</a></div>
-				<div class="menu_emergencies">For Emergencies: +563 47558 623</div>
-			</div>
+</header>
 
-		</div>
+<!-- Menu -->
 
-	</div>
-    
+<div class="menu_container menu_mm">
+
+<!-- Menu Close Button -->
+<div class="menu_close_container">
+    <div class="menu_close"></div>
+</div>
+
+<!-- Menu Items -->
+<div class="menu_inner menu_mm">
+    <div class="menu menu_mm">
+        <ul class="menu_list menu_mm">
+            <li class="menu_item menu_mm"><a href="index.html">Home</a></li>
+            <li class="menu_item menu_mm"><a href="about.html">About us</a></li>
+            <li class="menu_item menu_mm"><a href="services.html">Services</a></li>
+            <li class="menu_item menu_mm"><a href="news.html">News</a></li>
+            <li class="menu_item menu_mm"><a href="contact.html">Contact</a></li>
+        </ul>
+    </div>
+    <div class="menu_extra">
+        <div class="menu_appointment"><a href="PHP\login.php">Request an Appointment</a></div>
+        <div class="menu_emergencies">For Emergencies: +563 47558 623</div>
+    </div>
+
+</div>
+
+</div>
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="styles/bootstrap4/popper.js"></script>
 <script src="styles/bootstrap4/bootstrap.min.js"></script>
