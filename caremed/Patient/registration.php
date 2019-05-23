e1d994f267b576d5669b972d2378be75f5080641
Fortesa <fortesa.hysenaj@studentet.uni-pr.edu>
2019-05-23 14:13:28 +0200

    Update caremed/Patient/registration.php

--- caremed/Patient/registration.php
+++ caremed/Patient/registration.php
@@ -23,7 +23,7 @@ if($query)
 <html lang="en">
 
 	<head>
-		<title>User Registration</title>
+		<title>Patient</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
