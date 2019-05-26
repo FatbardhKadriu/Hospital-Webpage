0f7fa2417f84588c96ce54b21f3a3205b7a46905
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-05-26 04:03:39 +0200

    Font Update

--- caremed/Patient/registration.php
+++ caremed/Patient/registration.php
@@ -64,7 +64,7 @@ if($query)
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<h1 style="color: #000000; font-size: 50px;">Patient Registration</h1>
+				<h1 style="color: #000000; font-size: 45px;font-weight:bold;">Patient Registration</h1>
 				</div>
 				<!-- start: REGISTER BOX -->
 				<div class="box-register">
