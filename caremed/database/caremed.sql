bb71e21af947d0ee4253b6c7574b09dd4748745a
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-05-26 03:36:41 +0200

    Update caremed.sql

--- caremed/database/caremed.sql
+++ caremed/database/caremed.sql
@@ -35,6 +35,7 @@ CREATE TABLE `doctors` (
   `id` int(11) NOT NULL primary key,
   `specilization` varchar(255) NOT NULL,
   `doctorName` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
   `address` longtext NOT NULL,
   `docFees` varchar(255) NOT NULL,
   `contactno` bigint(11) NOT NULL,
@@ -44,13 +45,14 @@ CREATE TABLE `doctors` (
   `updationDate` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
-INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `updationDate`) VALUES
-(1, 'Dentist', 'Doctor 1', 'Address 1', '500', 045111222, 'doc1@gmail.com', '123456', ''),
-(2, 'physiotherapist', 'Doctor 2', 'Address 2', '600', 045333444, 'doc2@gmail.com', '123456', ''),
-(3, 'General Physician', 'Doctor 3', 'Address 3', '1200', 045323123, 'doc3@gmail.com', '123456', ''),
-(4, 'Othorino-laringologist', 'Doctor 4', 'Address 4', '700', 045123211, 'doc4@gmail.com', '123456', ''),
-(5, 'Dermatologist', 'Doctor 5', 'Address 5', '900', 045444111, 'doc5@gmail.com', '123456', ''),
-(6, 'General Physician', 'Doctor 6', 'Address 6', '2500', 044191919, 'doc6@gmail.com', '123456', '');
+INSERT INTO `doctors` (`id`, `specilization`, `doctorName`,`gender`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `updationDate`) VALUES
+(1, 'Dentist', 'Doctor 1','male', 'Address 1', '500', 045111222, 'doc1@gmail.com', '123456', ''),
+(2, 'physiotherapist', 'Doctor 2','male', 'Address 2', '600', 045333444, 'doc2@gmail.com', '123456', ''),
+(3, 'General Physician', 'Doctor 3', 'female','Address 3', '1200', 045323123, 'doc3@gmail.com', '123456', ''),
+(4, 'Othorino-laringologist', 'Doctor 4','female', 'Address 4', '700', 045123211, 'doc4@gmail.com', '123456', ''),
+(5, 'Dermatologist', 'Doctor 5', 'male','Address 5', '900', 045444111, 'doc5@gmail.com', '123456', ''),
+(6, 'General Physician', 'Doctor 6','female', 'Address 6', '2500', 044191919, 'doc6@gmail.com', '123456', '');
+
 
 CREATE TABLE `doctorslog` (
   `id` int(11) NOT NULL primary key,
@@ -100,8 +102,8 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`) VALUES
-(6, 'Patient1', 'Address 1', 'City 1', 'Males', 'pat1@gmail.com', '123456'),
-(7, 'Patient2', 'Address 2', 'City 2', 'Females', 'pat2@gmail.com', '123456');
+(6, 'Patient1', 'Address 1', 'City 1', 'male', 'pat1@gmail.com', '123456'),
+(7, 'Patient2', 'Address 2', 'City 2', 'female', 'pat2@gmail.com', '123456');
 
 
 ALTER TABLE `admin`
