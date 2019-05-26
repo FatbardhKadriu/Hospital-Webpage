67e4ec96cec60703929b4717bdcfba5cf96f81ea
Fortesa <fortesa.hysenaj@studentet.uni-pr.edu>
2019-05-26 03:52:17 +0200

    Doc-LogIn

---
+++ caremed/Doctor/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include('include/config.php');
+$_SESSION['dlogin']=="";
+date_default_timezone_set('Europe/Amsterdam');
+$ldate=date( 'd-m-Y h:i:s A', time () );
+mysqli_query($con,"UPDATE doctorslog  SET logout = '$ldate' WHERE uid = '".$_SESSION['id']."' ORDER BY id DESC LIMIT 1");
+session_unset();
+//session_destroy();
+?>
+<script language="javascript">
+document.location="index.php";
+</script>
