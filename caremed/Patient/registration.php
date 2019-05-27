6c1d21fd91ec55bd2a9fda3f92a6d02f12fd6071
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-05-28 00:05:18 +0200

    Update registration.php

--- caremed/Patient/registration.php
+++ caremed/Patient/registration.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include_once('include/config.php');
-$_SESSION['Passwordsdonotmatch']="";
 $passwordErr = "";
 $cpasswordErr = "";
 function test_input($data) {
@@ -23,35 +22,22 @@ if(isset($_POST['submit']))
 	$_SESSION['fullName'] = $fname;
 
 	if(!empty($_POST["password"]) && ($_POST["password"] == $_POST["cpassword"])) {
-        $password = test_input($_POST["password"]);
-        $cpassword = test_input($_POST["cpassword"]);
-        if (strlen($_POST["password"]) <= 8) {
-            $passwordErr = "Your Password Must Contain At Least 8 Characters!";
-        }
-        elseif(!preg_match("#[0-9]+#",$password)) {
-            $passwordErr = "Your Password Must Contain At Least 1 Number!";
-        }
-        elseif(!preg_match("#[A-Z]+#",$password)) {
-            $passwordErr = "Your Password Must Contain At Least 1 Capital Letter!";
-        }
-        elseif(!preg_match("#[a-z]+#",$password)) {
-            $passwordErr = "Your Password Must Contain At Least 1 Lowercase Letter!";
+        if (strlen($_POST["password"]) <= 8 || !preg_match("#[0-9]+#",$password) || !preg_match("#[A-Z]+#",$password) || !preg_match("#[a-z]+#",$password)) {
+						$passwordErr = "Your Password Must Contain At Least 8 Characters, 1 Number, 1Capital Letter, 1 Lowercase Letter!";
+				}
+				else{
+					$query=mysqli_query($con,"insert into users(fullname,address,city,gender,email,password) values('$fname','$address','$city','$gender','$email','$password')");
+					if($query)
+					{
+						header('location: registrationSuccessful.php');
+					}
+
+				}
 		}
-		
-    }
-    elseif(!empty($_POST["password"])) {
-		$cpasswordErr = "Please Check You've Entered Or Confirmed Your Password!";
-		
-    } else {
-		 
-		$query=mysqli_query($con,"insert into users(fullname,address,city,gender,email,password) values('$fname','$address','$city','$gender','$email','$password')");
-		if($query)
-		{
-			header('location: registrationSuccessful.php');
+		else{
+			$passwordErr = "Your password don't match";
 		}
-		 
-	}
-	
+
 }
 ?>
 
