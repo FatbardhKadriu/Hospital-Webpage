17e9c31f3fecbee27831f0d7a1ec888acf919ed2
FatbardhGashi <48059724+FatbardhGashi@users.noreply.github.com>
2019-06-01 17:46:46 +0200

    Update index.php

--- caremed/Pharmacy/index.php
+++ caremed/Pharmacy/index.php
@@ -119,7 +119,50 @@ include "action.php";
 </tr>
 
 
+
+
 <?php
 }
 
 ?>
+<?php
+
+
+	class Doctor
+	{
+		private $name;
+
+		public function __construct($name)
+		{
+			echo ("The doctor's name is : $name");
+		}
+
+		public function get_name()
+		{
+			return $this->name;
+		}
+
+		public function set_name($name)
+		{
+			$this->name = $name;
+		}
+
+
+	}
+
+	$doc1 = new Doctor("Doctor1<br/>");
+
+	echo $doc1->get_name();
+
+?>
+
+<script type="text/javascript">
+	function myFunction()
+	{
+		document.getElementById("demo").innerHTML = "<?php echo "Now the doctor's name is :".$doc1->set_name("Doctor2");	
+																echo $doc1->get_name();?>";
+	}
+</script>
+	<button class="DocNameBtn" type="button" onclick="myFunction()">See Doctor's Name: </button>
+
+	<p id="demo">Hello</p>
