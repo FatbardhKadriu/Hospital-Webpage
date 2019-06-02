cf7cbb7de177f0a2d6c6c1f69116010c18e46cad
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-06-02 16:10:35 +0200

    try catch

--- caremed/Doctor/include/config.php
+++ caremed/Doctor/include/config.php
@@ -5,10 +5,10 @@ $db_username = $_ENV['DB_USERNAME'];
 $db_password = $_ENV['DB_PASSWORD'];
 $db_name = $_ENV['DB_PROJECTNAME'];
 
-$con = mysqli_connect($db_host,$db_username,$db_password,$db_name);
-// Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+try {
+    $con = mysqli_connect($db_host,$db_username,$db_password,$db_name);
+}catch (Exception $e){
+    $error = $e->getMessage();
+    echo $error;
 }
 ?>
